ca84cea535642102e9d1a9d4476b9c2bd920e4fb
Kihong Heo <kihong.heo@kaist.ac.kr>
2026-05-28 12:31:02 +0900

    update

--- nvim/lua/plugins/lean.lua
+++ nvim/lua/plugins/lean.lua
@@ -4,6 +4,19 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
-    opts = { mappings = true }
+    opts = { mappings = true },
+    config = function()
+      require("lean").setup({
+        lsp = {
+          semanticHighlighting = false,
+        }
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "lean",
+        callback = function()
+          vim.opt_local.colorcolumn = "80"
+        end,
+    })
+  end,
   },
 }
